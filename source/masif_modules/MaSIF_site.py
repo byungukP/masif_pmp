a8020745c2269542250bf3b8cc36e3511bd2b99f
byungukP <90356818+byungukP@users.noreply.github.com>
2024-04-09 19:42:07 -0500

    tf.reduce_sum arg update

--- source/masif_modules/MaSIF_site.py
+++ source/masif_modules/MaSIF_site.py
@@ -101,7 +101,7 @@ class MaSIF_site(tf.keras.Model):
                 mean_gauss_activation
             ):  # computes mean weights for the different gaussians (gaussian-wise averaging)
                 gauss_activations /= (
-                    tf.reduce_sum(gauss_activations, 1, keep_dims=True) + eps
+                    tf.reduce_sum(gauss_activations, 1, keepdims=True) + eps
                 )  # batch_size, n_vertices, n_gauss
 
             gauss_activations = tf.expand_dims(
