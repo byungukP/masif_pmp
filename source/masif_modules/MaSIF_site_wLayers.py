12fe814322356641cba5994169e4d26710fdb6d8
byungukP <90356818+byungukP@users.noreply.github.com>
2024-04-29 19:52:52 -0500

    debug

--- source/masif_modules/MaSIF_site_wLayers.py
+++ source/masif_modules/MaSIF_site_wLayers.py
@@ -214,6 +214,7 @@ class MaSIF_site(L.LightningModule):
         if self.n_conv_layers > 1:
             print("global_desc shape before gather: {}".format(global_desc.shape))
             print("vert1234 surface desc before gather: {}".format(global_desc[1234,:]))
+            print(global_desc)
             # Rebuild a patch based on the output of the first layer
             global_desc = global_desc[self.indices_tensor]
             # global_desc = torch.gather(
