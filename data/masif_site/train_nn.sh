224f34f049182a0fbb126eadd3f32cbdd11bc155
byungukP <90356818+byungukP@users.noreply.github.com>
2024-05-06 11:01:29 -0500

    git access works on HTC server. So let's use $(git rev-parse --show-toplevel) command to parse the git root directyory

--- data/masif_site/train_nn.sh
+++ data/masif_site/train_nn.sh
@@ -1,6 +1,6 @@
-masif_root=/masif_pmp    # root_dir manually defined based on last WORKDIR in Dockerfile
-# masif_root=$(git rev-parse --show-toplevel)      # causing ownership error: "fatal: detected dubious ownership in repository at '/masif' To add an exception for this directory, call: git config --global --add safe.directory /masif"
-                                                  # when tried the command --> error: could not lock config file //.gitconfig: Permission denied
+# masif_root=/masif_pmp    # root_dir manually defined based on last WORKDIR in Dockerfile
+masif_root=$(git rev-parse --show-toplevel)
+
 masif_source=$masif_root/source/
 masif_data=$masif_root/data/
 export PYTHONPATH=$PYTHONPATH:$masif_source:$masif_data/masif_site/
@@ -10,5 +10,3 @@ date
 python3 $masif_source/masif_site/masif_site_train.py $1
 echo "Training Done:"
 date
-
-# git pull is working on HTC!!!
