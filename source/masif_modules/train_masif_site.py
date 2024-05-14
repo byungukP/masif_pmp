9011a1cad620345d3867d80d538a6f686a7fb98b
byungukP <90356818+byungukP@users.noreply.github.com>
2024-05-14 14:16:56 -0500

    numpy compatibility issue edits

--- source/masif_modules/train_masif_site.py
+++ source/masif_modules/train_masif_site.py
@@ -343,6 +343,8 @@ def train_masif_site(
             output_model = out_dir + "model.pt"
             torch.save(model.state_dict(), output_model)
             # Save the scores for test.
+            all_test_labels = np.array(all_test_labels, dtype=object)  # Explicitly change the dtype to object
+            
             np.save(out_dir + "test_labels.npy", all_test_labels)
             np.save(out_dir + "test_scores.npy", all_test_scores)
             np.save(out_dir + "test_names.npy", list_test_names)
