7355d57e5a3a38119d24aef6f4600b0e1eeed5e5
byungukP <90356818+byungukP@users.noreply.github.com>
2024-08-09 14:05:13 -0500

    new script added - for handling PDB files generated using HTM - check whether origianl workflow using reduce for protonation, extractPDB for extraction work well with HTMD-generated conformational ensemble

--- source/input_output/extractPDB.py
+++ source/input_output/extractPDB.py
@@ -35,8 +35,6 @@ def extractPDB(
     # extract the chain_ids from infilename and save in outfilename. 
     parser = PDBParser(QUIET=True)
     struct = parser.get_structure(infilename, infilename)
-    print(struct)
-    print(Selection.unfold_entities(struct, "C"))
     model = Selection.unfold_entities(struct, "M")[0]
     chains = Selection.unfold_entities(struct, "C")
     # Select residues to extract and build new structure
