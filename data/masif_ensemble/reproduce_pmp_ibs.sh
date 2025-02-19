261fb07214f7ddfb95b6bdc66bb00ad75f027097
byungukP <90356818+byungukP@users.noreply.github.com>
2025-02-19 17:20:42 -0600

    new directory added for masif_ensemble

---
+++ data/masif_ensemble/reproduce_pmp_ibs.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cat lists/pmp_full_list.txt | while read line 
+do
+    ./data_prepare_one_ibs.sh $line\_
+    # ./data_prepare_one_ibs_ensemble.sh $line\_ --ensemble
+done
+
+./predict_site.sh -l lists/pmp_full_list.txt
+./color_site.sh -l lists/pmp_full_list.txt
---
+++ data/masif_ensemble/train_nn.sh
@@ -0,0 +1,12 @@
+masif_root=/masif_pmp    # root_dir manually defined based on last WORKDIR in Dockerfile
+# masif_root=$(git rev-parse --show-toplevel)      # causing ownership error: "fatal: detected dubious ownership in repository at '/masif' To add an exception for this directory, call: git config --global --add safe.directory /masif"
+                                                  # when tried the command --> error: could not lock config file //.gitconfig: Permission denied
+masif_source=$masif_root/source/
+masif_data=$masif_root/data/
+export PYTHONPATH=$PYTHONPATH:$masif_source:$masif_data/masif_site/
+
+echo "Training Start:"
+date
+python3 $masif_source/masif_site/masif_site_train.py $1
+echo "Training Done:"
+date
