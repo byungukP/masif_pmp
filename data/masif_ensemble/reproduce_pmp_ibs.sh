6cc5a24071a3158b6c7361b48b920298a52b3c78
byungukP <90356818+byungukP@users.noreply.github.com>
2025-02-19 17:28:04 -0600

    updates

--- data/masif_ensemble/reproduce_pmp_ibs.sh
+++ data/masif_ensemble/reproduce_pmp_ibs.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-cat lists/pmp_full_list.txt | while read line 
+cat lists/pmp_5cv_trainset_low_auc_100_noOutlier.txt | while read line 
 do
-    ./data_prepare_one_ibs.sh $line\_
-    # ./data_prepare_one_ibs_ensemble.sh $line\_ --ensemble
+    ./data_prepare_one_ibs_ensemble.sh $line\_ --ensemble
 done
 
-./predict_site.sh -l lists/pmp_full_list.txt
-./color_site.sh -l lists/pmp_full_list.txt
+./predict_site.sh -l lists/pmp_5cv_trainset_low_auc_100_noOutlier.txt
+./color_site.sh -l lists/pmp_5cv_trainset_low_auc_100_noOutlier.txt
--- data/masif_site/reproduce_pmp_ibs.sh
+++ data/masif_site/reproduce_pmp_ibs.sh
@@ -2,7 +2,6 @@
 cat lists/pmp_full_list.txt | while read line 
 do
     ./data_prepare_one_ibs.sh $line\_
-    # ./data_prepare_one_ibs_ensemble.sh $line\_ --ensemble
 done
 
 ./predict_site.sh -l lists/pmp_full_list.txt
