3d30ea21dc452353e1abfb15c00721663ccb5405
byungukP <90356818+byungukP@users.noreply.github.com>
2025-02-27 15:27:52 -0600

    edit for merge

--- data/masif_ensemble copy/README.md
+++
@@ -1,6 +0,0 @@
-### data/masif_ensemble
-
-Containts commands to run MaSIF-ensemble experiments.
-
-To run the transient benchmark run: 
-./reproduce_transient_benchmark.sh
--- data/masif_ensemble copy/reproduce_pmp_ibs.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cat lists/pmp_5cv_trainset_low_auc_100_noOutlier.txt | while read line 
-do
-    ./data_prepare_one_ibs_ensemble.sh $line\_ --ensemble
-done
-
-./predict_site.sh -l lists/pmp_5cv_trainset_low_auc_100_noOutlier.txt
-./color_site.sh -l lists/pmp_5cv_trainset_low_auc_100_noOutlier.txt
