bc4f0aea34f0ca550994d528afaadbbf1b3d78b9
byungukP <90356818+byungukP@users.noreply.github.com>
2025-04-26 19:03:20 -0500

    revised train_nn.sh for ensemble_ppi

--- data/masif_ens_ppi/train_nn.sh
+++ data/masif_ens_ppi/train_nn.sh
@@ -1,12 +1,11 @@
-# masif_root=/masif_pmp    # root_dir manually defined based on last WORKDIR in Dockerfile
 masif_root=$(git rev-parse --show-toplevel)
 
 masif_source=$masif_root/source/
 masif_data=$masif_root/data/
-export PYTHONPATH=$PYTHONPATH:$masif_source:$masif_data/masif_site/
+export PYTHONPATH=$PYTHONPATH:$masif_source:$masif_data/masif_ensemble/
 
 echo "Training Start:"
 date
-python3 $masif_source/masif_site/masif_site_train.py $1
+python3 $masif_source/masif_ensemble/masif_ensemble_train.py $1
 echo "Training Done:"
 date
