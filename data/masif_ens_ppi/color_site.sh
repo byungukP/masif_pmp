a4f2002494a6bc9a42561da587e8c8caf1fb1a14
byungukP <90356818+byungukP@users.noreply.github.com>
2025-04-28 13:12:26 -0500

    edited for masif_ens_ppi

--- data/masif_ens_ppi/color_site.sh
+++ data/masif_ens_ppi/color_site.sh
@@ -3,5 +3,6 @@ masif_root=$(git rev-parse --show-toplevel)
 masif_source=$masif_root/source/
 masif_matlab=$masif_root/source/matlab_libs/
 masif_data=$masif_root/data/
-export PYTHONPATH=$PYTHONPATH:$masif_source:$masif_data/masif_site/
-python -W ignore $masif_source/masif_site/masif_site_label_surface.py nn_models.all_feat_3l.custom_params $1 $2
+export PYTHONPATH=$PYTHONPATH:$masif_source:$masif_data/masif_ensemble/
+# python -W ignore $masif_source/masif_ensemble/masif_ensemble_label_surface.py nn_models.all_feat_3l.custom_params $1 $2
+python -W ignore $masif_source/masif_ensemble/masif_ensemble_label_surface.py $1 $2 $3
