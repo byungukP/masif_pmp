7d4df08ed0c2099283962dcb089450434a63fafa
byungukP <90356818+byungukP@users.noreply.github.com>
2025-07-22 16:49:30 -0500

    removed unused directory

--- data/masif_ens_ppi/predict_site.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-masif_root=$(git rev-parse --show-toplevel)
-masif_source=$masif_root/source/
-masif_matlab=$masif_root/source/matlab_libs/
-masif_data=$masif_root/data/
-export PYTHONPATH=$PYTHONPATH:$masif_source:$masif_data/masif_ensemble/
-# python -W ignore $masif_source/masif_ensemble/masif_ensemble_predict.py nn_models.all_feat_3l.custom_params $1 $2
-python -W ignore $masif_source/masif_ensemble/masif_ensemble_predict.py $1 $2 $3
